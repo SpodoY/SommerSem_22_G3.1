611832861a85774c19e977e9b7e6b2c8fb4b32fb
Christina Moser <moser.christina@outlook.at>
2022-03-16 13:50:27 +0100

    removed everything from me for deep fake :D

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,10 +2,6 @@ package at.ac.fhcampuswien;
 
 public class App {
 
-    public static void main(String[] args) {
-        // launches application
-        Menu menu = new Menu();
-        menu.start();
-    }
+
 
 }
