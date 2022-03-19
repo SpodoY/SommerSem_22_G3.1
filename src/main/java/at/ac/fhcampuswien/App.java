9da849d32f43cecd814bef6de78c4a44bb82e217
ChristinaMoser <christina.moser1@stud.fh-campuswien.ac.at>
2022-03-19 13:32:07 +0100

    completed App - menu.start();

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,6 +2,11 @@ package at.ac.fhcampuswien;
 
 public class App {
 
+    public static void main(String[] args) {
+        // launches application
+        Menu menu = new Menu();
+        menu.start();
 
+    }
 
 }
