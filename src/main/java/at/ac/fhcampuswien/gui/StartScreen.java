e733c9504dde81de432a7ca44694a08de01a4d33
Thomas Meier <thommy2002@outlook.com>
2022-03-19 11:39:57 +0100

    GUI lil bit prettier

--- src/main/java/at/ac/fhcampuswien/gui/StartScreen.java
+++ src/main/java/at/ac/fhcampuswien/gui/StartScreen.java
@@ -2,8 +2,10 @@ package at.ac.fhcampuswien.gui;
 
 import at.ac.fhcampuswien.AppController;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -23,6 +25,10 @@ public class StartScreen {
     public static void main(String[] args) {
 
         Platform.startup(() -> {
+
+            Label welcomeText = new Label("News App - G3.1");
+            welcomeText.setStyle("-fx-font: 36 Verdana");
+
             Button atButton = new Button("AT News");
             Button bitButton = new Button("Bitcoin News");
             Button countButton = new Button("Article Count");
@@ -33,7 +39,15 @@ public class StartScreen {
             countButton.setOnAction(e -> loadNewScene("Count"));
             quitButton.setOnAction(e -> loadNewScene("Fuck this shit I'm out"));
 
-            CONTAINER.getChildren().addAll(atButton, bitButton, countButton, quitButton);
+            atButton.setMinWidth(WIDTH); atButton.setMinHeight(75);
+            bitButton.setMaxWidth(Double.MAX_VALUE); bitButton.setMinHeight(75);
+            countButton.setMaxWidth(Double.MAX_VALUE); countButton.setMinHeight(75);
+            quitButton.setMaxWidth(Double.MAX_VALUE); quitButton.setMinHeight(75);
+
+            CONTAINER.setSpacing(40);
+            CONTAINER.setAlignment(Pos.CENTER);
+
+            CONTAINER.getChildren().addAll(welcomeText, atButton, bitButton, countButton, quitButton);
 
             mainScene = new Scene(CONTAINER, WIDTH, HEIGHT);
 
