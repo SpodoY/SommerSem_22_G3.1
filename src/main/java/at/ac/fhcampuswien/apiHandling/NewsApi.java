0fc814c5bf6f4f28c0f2d50ad71eddc88365d859
Maxl <maximilian.nowak@outlook.at>
2022-03-20 09:48:39 +0100

    added a bit of dependency

--- src/main/java/at/ac/fhcampuswien/apiHandling/NewsApi.java
+++ src/main/java/at/ac/fhcampuswien/apiHandling/NewsApi.java
@@ -1,7 +1,13 @@
 package at.ac.fhcampuswien.apiHandling;
 
+
 public class NewsApi {
     private String baseQuery = "https://newsapi.org/v2/";
     private String topHeadlines ="top-headlines";
     private String everything = "everything";
+
+    private void testResponse () {
+
+    }
+
 }
