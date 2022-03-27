df40d99e780defdcd095c7845bfbe8b9314a8fb6
Thomas Meier <thommy2002@outlook.com>
2022-03-27 15:40:24 +0200

    Commented Menu

--- src/main/java/at/ac/fhcampuswien/Menu.java
+++ src/main/java/at/ac/fhcampuswien/Menu.java
@@ -26,6 +26,11 @@ public class Menu {
 
     }
 
+    /**
+     * Based on which String is given the corresponding method gets called.
+     * If the input is wrong a message is displayed
+     * @param input The string to tell which method should be called
+     */
     private void handleInput(String input) {
         // input handling via console input
         switch (input) {
@@ -60,12 +65,18 @@ public class Menu {
         System.out.println(INVALID_INPUT_MESSAGE);
     }
 
+    /**
+     * Prints the menu as stated in the specifications
+     */
     private static void printMenu() {
         final int REPEAT_COUNT = 30;
+        // String with the 4 Options the user can choose
         String[] options = new String[]{"Get top headlines Austria", "Get all news about bitcoin", "Count articles", "Quit program"};
 
+        /* Prints '*' REPEAT_COUNT times, then a new line with the text and then prints '*' again in the third line */
         System.out.printf("%s%n%s%n%s%n", "*".repeat(REPEAT_COUNT), "*     Welcome to NewsApp     *", "*".repeat(REPEAT_COUNT));
         System.out.println("Enter what you wanna do: ");
+        /* Prints the four options which are written in the options[] */
         System.out.printf("a: %s%n" + "b: %s%n" + "y: %s%n" + "q: %s%n", options[0], options[1], options[2], options[3]);
     }
 
