9637e184cbc80bf03b02c2aed940f8e27bf58044
Chronikle <Simon.Nenning@gmx.at>
2022-05-03 11:41:50 +0200

    Implemented streams for length of author and how often new york times is the publisher

--- src/main/java/at/ac/fhcampuswien/Article.java
+++ src/main/java/at/ac/fhcampuswien/Article.java
@@ -32,6 +32,10 @@ public class Article implements Comparable<Article> {
         return author;
     }
 
+    public int getAuthorLength(){
+        return author.length();
+    }
+
     public String getTitle() {
         return title;
     }
@@ -71,5 +75,6 @@ public class Article implements Comparable<Article> {
             return Integer.compare(a.getDescription().length(), getDescription().length());
         }
     }
+
 }
 
