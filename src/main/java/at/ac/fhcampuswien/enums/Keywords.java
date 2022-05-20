0f1c85651bd138bdbd0423f98469bc9600996b92
megamxl <maximilian.nowak@outlook.at>
2022-05-20 13:41:07 +0200

    added keyword enums

---
+++ src/main/java/at/ac/fhcampuswien/enums/Keywords.java
@@ -0,0 +1,27 @@
+package at.ac.fhcampuswien.enums;
+
+public enum Keywords {
+
+    CRYPTO("q=crypto"),
+    APPLE("q=apple"),
+    SAMSUNG("q=samsung"),
+    TESLA("q=tesla"),
+    STOCKS("q=stocks"),
+    HUAWEI("q=huawei"),
+    TWITTER("q=twitter"),
+    AMAZON("q=amazon"),
+    GOOGEL("q=googel");
+
+
+    private final String partOfUrl;
+
+    Keywords (final String partOfUrl){
+        this.partOfUrl = partOfUrl;
+    }
+
+    @Override
+    public String toString() {
+        return this.partOfUrl;
+    }
+
+}
